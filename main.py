ea62c4f16c511ae0660ace1cb98cd4b928d2e2fd
irfan <airifunandirk@gmail.com>
2020-10-14 02:11:19 +0700

    Percabangan (if else)

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ print('Hello World!')
 print('by Irfan')
 print('tanggal 14 Okt 2020')
 print('-' * 10)
+
+# Percabangan: Eksekusi Terpilih
+
+ingin_cepat = True
+if ingin_cepat:
+    print('jalan lurus aja ya!')
+else:
+    print('Jalan lain')
