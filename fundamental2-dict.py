60c1efd5037c72fe78e866acd8a60abdc51a3af5
irfan <airifunandirk@gmail.com>
2020-11-17 21:13:33 +0700

    tipe data dictionary

---
+++ fundamental2-dict.py
@@ -0,0 +1,27 @@
+"""
+Tipe data Dictionary
+Menghubungkan antara KEY dan VALUE
+"""
+
+kamus_ind_eng = {'anak': 'son', 'istri': 'wife', 'ayah': 'father', 'ibu': 'mother'}
+
+print(kamus_ind_eng)
+print(kamus_ind_eng['ayah'])
+print(kamus_ind_eng['ibu'])
+#print(kamus_ind_eng['mother'])
+
+print('Data ini dikirimkan oleh server Gojek, untuk memberikan info driver di sekitar pemakai aplikasi')
+data_dari_server_gojek = {
+    'tanggal':'2020-11-17',
+    'driver_list': [
+        {'nama': 'Eka','jarak':10},
+        {'nama': 'Dwi','jarak':30},
+        {'nama': 'Tri','jarak':100},
+        {'nama':'Catur','jarak':1000}
+    ]
+}
+print (data_dari_server_gojek)
+print(f"\nDriver Gojek di sekitar sini: {data_dari_server_gojek['driver_list']}")
+print(f"Driver Gojek #1: {data_dari_server_gojek['driver_list'][0]}")
+print(f"Driver Gojek #4: {data_dari_server_gojek['driver_list'][3]}")
+print(f"\nDriver terdekat berjarak {data_dari_server_gojek['driver_list'][0]['jarak']} meter")
